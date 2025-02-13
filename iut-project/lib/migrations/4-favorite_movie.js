ab5a2b2c9b6245626574dad29bbe9a051cceebc2
Yannis Boisserie <yannis.boisserie@etu.unilim.fr>
2025-02-13 09:38:11 +0100

    ajout migration favorite movie

---
+++ iut-project/lib/migrations/4-favorite_movie.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = {
+
+    async up(knex) {
+
+        await knex.schema.createTable('favorites', (table) => {
+
+            table.increments('id').primary();
+            table.integer('userId').unsigned().notNull().references('id').inTable('user').onDelete('CASCADE');
+            table.integer('movieId').unsigned().notNull().references('id').inTable('movies').onDelete('CASCADE');
+
+            table.dateTime('createdAt').notNull().defaultTo(knex.fn.now());
+        });
+    },
+
+    async down(knex) {
+
+        await knex.schema.dropTableIfExists('favorites');
+    }
+};
